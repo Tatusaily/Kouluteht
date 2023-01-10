528d9fcaf2688b89e5a045bf16ca46ef2b2b877e
Tatu <tatu.sailynoja@gmail.com>
2023-01-10 18:12:20 +0200

    voi optimoida

---
+++ Harjoitustehtävät/Teht2/2.3 Suorakulmio.py
@@ -0,0 +1,11 @@
+#Otetaan suorakulmion kanta ja korkeus
+print("Anna kanta")
+kanta = int(input())
+print("Anna korkeus")
+korkeus = int(input())
+
+#Tulostetaan piiri ja pinta-ala
+pinta = kanta*korkeus
+piiri = kanta*2+korkeus*2
+print("Suorakulmion pinta-ala on:", pinta)
+print("Suorakulmion piiri on:", piiri)
