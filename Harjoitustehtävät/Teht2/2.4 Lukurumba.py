09af3a63408f2afc78c2d8e1354b7d97cb0bdd94
Tatu <tatu.sailynoja@gmail.com>
2023-01-11 19:50:18 +0200

    Aritmeettinen lukukone

---
+++ Harjoitustehtävät/Teht2/2.4 Lukurumba.py
@@ -0,0 +1,15 @@
+import math
+#Kysy kolme kokonaislukua.
+eka=            int(input("Anna ensimmäinen luku:"))
+toka=           int(input("Anna toinen luku:"))
+kolmas=         int(input("Anna kolmas luku:"))
+
+#Matikat:
+summa =         (eka+toka+kolmas)
+tulo =          (eka*toka*kolmas)
+keskiarvo =     summa/3
+
+#Tulosta lukujen summa, tulo ja keskiarvo.
+print("Lukujen summa on:", summa)
+print("Lukujen tulo on:", tulo)
+print("Lukujen keskiarvo on:", keskiarvo)
