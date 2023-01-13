c3aabc376762d3a795e5589970d287ec7bed6366
Tatu <tatu.sailynoja@gmail.com>
2023-01-13 18:24:26 +0200

    muutin muutujien nimiä

--- Harjoitustehtävät/Teht2/2.5 Massat.py
+++ Harjoitustehtävät/Teht2/2.5 Massat.py
@@ -1,11 +1,12 @@
 import math
 #Kysy yksiköt
-leiviskäIn=     float(input("Montako leiviskää?"))
-naulaIn=        float(input("Montako naulaa?"))
-luotiIn=        float(input("Montako luotia?"))
+leiviska_in =    float(input("Montako leiviskää?"))
+naula_in =       float(input("Montako naulaa?"))
+luoti_in =       float(input("Montako luotia?"))
 
 #Lasku ja tulostus
-grammat=        luotiIn*13.3 + naulaIn*13.3*32 + leiviskäIn*13.3*32*20
-kilot=          math.floor(grammat/1000)
+grammat =       luoti_in*13.3 + naula_in*13.3*32 + leiviska_in*13.3*32*20
+kilot =         math.floor(grammat/1000)
 print("Massa kiloissa ja grammoissa:")
 print(kilot, "kiloa ja ", round(grammat-kilot*1000, 1), "grammaa.")
+round(x, y)
