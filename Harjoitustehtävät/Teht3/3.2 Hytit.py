bf40aedd59f8c38a571f9b14c8aed742ce872631
Tatusaily <tatu.sailynoja@gmail.com>
2023-01-13 18:19:41 +0200

    1

---
+++ Harjoitustehtävät/Teht3/3.2 Hytit.py
@@ -0,0 +1,20 @@
+#Kysyy hyttiluokan
+hyttivalinta = str.upper(input("Mikä on hyttiluokkasi?"))
+
+#Hyttien kuvaukset
+hyttilux = "LUX on parvekkeellinen hytti yläkannella."
+hyttia = "A on ikkunallinen hytti autokannen yläpuolella."
+hyttib = "B on ikkunaton hytti autokannen yläpuolella."
+hyttic = "C on ikkunaton hytti autokannen alapuolella."
+
+#Hyttikuvauksen anto
+if      hyttivalinta == "LUX":
+    print(hyttilux)
+elif    hyttivalinta == "A":
+    print(hyttia)
+elif    hyttivalinta == "B":
+    print(hyttib)
+elif    hyttivalinta == "C":
+    print(hyttic)
+else:
+    print("Virheellinen Hyttiluokka!")
