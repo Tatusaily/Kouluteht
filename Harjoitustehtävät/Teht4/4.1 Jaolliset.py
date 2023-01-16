7bde993f71cd7480dd3a14cc82a0f2588c4fb8d8
Tatu <tatu.sailynoja@gmail.com>
2023-01-16 10:33:07 +0200

    Siistimistä

--- Harjoitustehtävät/Teht4/4.1 Jaolliset.py
+++ Harjoitustehtävät/Teht4/4.1 Jaolliset.py
@@ -5,15 +5,20 @@ joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
 """
 n = 1
 yläraja = 1000
-x = 0
 jaolliset = []
+
+# 12-15 Ottaa n arvon ja katsoo onko se jaollinen kolmella. Jos on niin laittaa sen listaan.
 while n <= yläraja:
     if n % 3 == 0:
         jaolliset.append(n)
     n += 1
+
+# 18-23 Tulostaa jaolliset listan niin, että halutun (y) rivimäärän jälkeen tehdään rivinvaihdos.
+x = 1
+y = 5
 for i in jaolliset:
-    print(i,"", end = '') #end poistaa printin perästä sen automaattisen linebreakin.
+    print(i, "", end='')
     x += 1
-    if x == 3:            #printataan joka kolmas luku linebreak.
+    if x == y:
         print("")
         x = 0
