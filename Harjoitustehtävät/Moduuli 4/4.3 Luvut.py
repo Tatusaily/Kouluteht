fc0c1dd7695b35db34d0d0888228dad95499877a
Tatusaily <tatu.sailynoja@gmail.com>
2023-01-16 15:19:23 +0200

    .

--- Harjoitustehtävät/Moduuli 4/4.3 Luvut.py
+++ Harjoitustehtävät/Moduuli 4/4.3 Luvut.py
@@ -3,6 +3,7 @@ Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
 kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 """
+import ast
 
 lukujono = []
 x = 0
@@ -11,12 +12,10 @@ while (x == 0):
     uusiluku = input("Anna luku lukujonoon tai paina ENTER")
     if (uusiluku == ""):
         x = 1
+    elif ((type(uusiluku) is int) or (type(uusiluku) is float)):
+        lukujono.append(uusiluku)
     else:
-        try:
-            uusiluku = int(uusiluku)
-            lukujono.append(uusiluku)
-        except:
-            print("Uusi luku ei ollut numero!")
+        print("Uusi lukusi ei ollut luku. Koita uudestaan.")
 
 print(lukujono)
 #Tulosta pienin ja suurin.
