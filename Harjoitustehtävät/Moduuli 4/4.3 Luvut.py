3330e0efba76c89bd2ae4973a338c8ecd295df8f
Tatusaily <tatu.sailynoja@gmail.com>
2023-01-19 14:50:57 +0200

    1.0 toimii kaatuu jos käyttäjä syöttää kirjaimia! voi korjata try: except: lauseella.

--- Harjoitustehtävät/Moduuli 4/4.3 Luvut.py
+++ Harjoitustehtävät/Moduuli 4/4.3 Luvut.py
@@ -3,20 +3,14 @@ Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
 kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 """
-import ast
-
 lukujono = []
-x = 0
+uusiluku = input("Anna luku lukujonoon tai paina ENTER")
 
-#TODO laita tähän looppiin joku tunnistus käyttäjän inputille ja päättele voiko sen laittaa [lukujonoon].
-while (x == 0):
+while (uusiluku != ""):
+    uusiluku = float(uusiluku)
+    if uusiluku % 1 == 0.0:
+        uusiluku = int(uusiluku)
+    lukujono.append(uusiluku)
     uusiluku = input("Anna luku lukujonoon tai paina ENTER")
-    if (uusiluku == ""):
-        x = 1
-    elif ((type(uusiluku) is int) or (type(uusiluku) is float)):
-        lukujono.append(uusiluku)
-    else:
-        print("Uusi lukusi ei ollut luku. Koita uudestaan.")
 
-print(lukujono)
-#TODO Tulosta pienin ja suurin.
+print(f"Pienin luku on {min(lukujono)} ja suurin on {max(lukujono)}.")
