f1b8cdc1405c7bf0a77d6bad1218c8d8932fcae3
Tatu <tatu.sailynoja@gmail.com>
2023-01-23 19:35:11 +0200

    1.0

--- Harjoitustehtävät/Moduuli 6/6.1 Noppafunk.py
+++ Harjoitustehtävät/Moduuli 6/6.1 Noppafunk.py
@@ -3,4 +3,12 @@ Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan si
 Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
 Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
 """
-def
+import random
+noppa = 0
+def noppakuusi():
+    return random.randint(1, 6)
+
+while noppa != 6:
+    print("Heitetään noppaa:")
+    noppa = noppakuusi()
+    print(f"Nopan silmäluvuksi tuli: {noppa}\n")
