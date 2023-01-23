474d7f35640b3152ac636a87f3b0023ce7cecedd
Tatusaily <tatu.sailynoja@gmail.com>
2023-01-23 14:32:02 +0200

    korjattu

--- Harjoitustehtävät/Moduuli 4/4.2 TuumatSenteiksi.py
+++ Harjoitustehtävät/Moduuli 4/4.2 TuumatSenteiksi.py
@@ -5,18 +5,21 @@ Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
 """
 
 #Ota tuumat
-tuumat = input("Anna tuumat. ")
+tuumat = ""
 sentit = 0.0
-try:
-    tuumat = float(tuumat)
-except:
-    print("Anna tuumat numeroina!")
-    quit()
-#Onko negatiivinen?
-if tuumat < 0:
-    print("Ohjelma sammuu, koska tuumat ovat negatiivisia.")
-    quit()
-else:
-#Muuta senteiksi ja print
-    sentit = tuumat*2.54
-    print(f"{sentit:.2f}")
+x = True
+
+while x == True:
+    tuumat = input("Anna Tuumat: ")
+    try:
+        tuumat = float(tuumat)
+    except:
+        print("Anna tuumat numeroina!")
+        quit()
+    if tuumat < 0:
+        print("Ohjelma sammuu, koska tuumat ovat negatiivisia.")
+        break
+    sentit = tuumat * 2.54
+    print(f"{tuumat} Tuumaa = {sentit} senttiä.\n")
+sentit = tuumat * 2.54
+print(f"{tuumat} Tuumaa = {sentit} senttiä.")
