7e0abb4a3fc4ccdae796388a8a03f9999e43c053
Tatusaily <tatu.sailynoja@gmail.com>
2023-01-26 14:17:18 +0200

    funk

---
+++ Harjoitustehtävät/Moduuli 6/6.6 Pizza.py
@@ -0,0 +1,13 @@
+"""
+Kirjoita funktio, joka saa parametreinaan pyöreän pizzan halkaisijan senttimetreinä sekä pizzan hinnan euroina.
+Funktio laskee ja palauttaa pizzan yksikköhinnan euroina per neliömetri.
+Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä
+    ilmoittaa, kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
+Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+"""
+import math
+def pizzalaskuri(halkaisija, hinta):
+    pinta_ala = math.pi * halkaisija
+    hinta_ala = hinta / pinta_ala
+    return hinta_ala
+
