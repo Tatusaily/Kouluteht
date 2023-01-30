6cf4c5e0bc5a0bcd151307345f73894e1bc06d1c
Tatusaily <tatu.sailynoja@gmail.com>
2023-01-30 13:07:13 +0200

    toimii nyt

--- Harjoitustehtävät/Moduuli 7/7.1 Vuodenajat.py
+++ Harjoitustehtävät/Moduuli 7/7.1 Vuodenajat.py
@@ -4,18 +4,25 @@ jälkeen ohjelma tulostaa sitä vastaavan vuodenajan (kevät, kesä, syksy, talv
 Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
 Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
 """
+#Otetaan kuukausi nro
+#Katsotaan mihin vuodenaikaan kuukausi kuuluu.
+    #12, 1, 2 = Talvi [0]
+    #3, 4, 5 = Kevät [1]
+    #6, 7, 8 = Kesä [2]
+    #9, 10, 11 = Syksy [3]
+#Ota vuodenajat -listasta (kuukausi/3 -pyör. alas) indeksistä.
+    #12 = [0]
+#Print vuodenaika
 
+import math
 
-vuodenajat = ("Talvi", "Kevät", "Kesä", "Syksy")
-
-#Kysytään käyttäjältä kuukauden numero:
+vuodenajat = ["Talvi", "Kevät", "Kesä", "Syksy"]
 kuukausi = int(input("Anna kuukauden numero: "))
 
-#Katsotaan mihin vuodenaikaan kuukausi kuuluu.
-    #12, 1, 2 = Talvi
-    #3, 4, 5 = Kevät
-    #6, 7, 8 = Kesä
-    #9, 10, 11 = Syksy
+if kuukausi == 12:
+    vuodenaikanro = 0
+else:
+    vuodenaikanro = math.floor(kuukausi/3)
 
-#Tulostetaan vastaava vuodenaika.
-#Print(vuodenaika)
+vuodenaika = vuodenajat[vuodenaikanro]
+print (vuodenaika)
