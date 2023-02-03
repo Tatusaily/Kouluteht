1eb07b24d393ff37fe2b50b1782a2f8f6ceb5f75
Tatusaily <tatu.sailynoja@gmail.com>
2023-02-03 15:06:58 +0200

    ok

---
+++ Moduuli 8/8.1 Lentoasemahakija.py
@@ -0,0 +1,27 @@
+"""Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin.
+Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen
+ sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
+ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen."""
+
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='user1',
+         password='sala1',
+         autocommit=True
+         )
+
+def GetAirport(ICAO):
+    ICAO = ICAO.upper()
+    query = f"SELECT name, municipality from airport WHERE ident= '{ICAO}'"
+    kursori = yhteys.cursor()
+    kursori.execute(query)
+    tulos = kursori.fetchall()
+    return tulos
+
+koodi = input("Anna ICAO koodi: ")
+print(GetAirport(koodi))
+
