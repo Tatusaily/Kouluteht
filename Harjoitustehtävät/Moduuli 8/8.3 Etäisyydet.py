2232496499c8d5baf40ba3076e9895fc8c1f2dff
Tatusaily <tatu.sailynoja@gmail.com>
2023-02-05 15:02:56 +0200

    0.2

--- Harjoitustehtävät/Moduuli 8/8.3 Etäisyydet.py
+++ Harjoitustehtävät/Moduuli 8/8.3 Etäisyydet.py
@@ -6,4 +6,24 @@ Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/.
 Asenna kirjasto valitsemalla View / Tool Windows / Python Packages.
 Kirjoita hakukenttään geopy ja vie asennus loppuun.
 """
+import geopy.distance
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='user1',
+         password='sala1',
+         autocommit=True
+         )
+
+def GetAirportCoords(ICAO):
+    ICAO = ICAO.upper()
+    query = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident= '{ICAO}';"
+    cursor = yhteys.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
+
 
