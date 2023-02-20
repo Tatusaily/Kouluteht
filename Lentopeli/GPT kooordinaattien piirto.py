f7447f8e9afdb18f2cdfaf069881a5a6dfbb65b0
Tatu <tatu.sailynoja@gmail.com>
2023-02-20 19:51:17 +0200

    nyt punainen kun painaa

--- Lentopeli/GPT kooordinaattien piirto.py
+++ Lentopeli/GPT kooordinaattien piirto.py
@@ -22,18 +22,25 @@ points = [(random.randint(0, screen_width), random.randint(0, screen_height)) fo
 # Define a function to draw the circles and lines
 def draw_points(points):
     # Draw the circles
-    for point in points:
-        pygame.draw.circle(screen, (255, 255, 255), point, circle_radius)
+    for i, point in enumerate(points):
+        color = (255, 255, 255)
+        if selected[i]:
+            color = (255, 0, 0)
+        pygame.draw.circle(screen, color, point, circle_radius)
 
     # Draw the lines
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
             distance = math.sqrt((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2)
             if distance <= line_radius:
-                pygame.draw.line(screen, (255, 255, 255), points[i], points[j], 1)
+                color = (255, 255, 255)
+                if selected[i] and selected[j]:
+                    color = (255, 0, 0)
+                pygame.draw.line(screen, color, points[i], points[j], 1)
 
 
 # Draw the initial points
+selected = [False] * num_points
 draw_points(points)
 
 # Update the display
@@ -45,3 +52,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            for i, point in enumerate(points):
+                distance = math.sqrt((pos[0]-point[0])**2 + (pos[1]-point[1])**2)
+                if distance <= circle_radius:
+                    selected[i] = not selected[i]
+            draw_points(points)
+            pygame.display.update()
