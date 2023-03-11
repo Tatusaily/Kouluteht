10321516bda10e4d5fe949150076cb9d9af15417
Tatu <tatu.sailynoja@gmail.com>
2023-03-10 16:06:17 -0800

    A

--- Muut/Erikoinen/main.py
+++ Muut/Erikoinen/main.py
@@ -1,2 +1,11 @@
+import pygame
+
 if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    screen.fill("Black")
     print("Main")
+
+    while True:
+        pygame.display.update()
+
