9885de7e6de89216f1ee8ccd072a017a08a15b1f
Tatu <tatu.sailynoja@gmail.com>
2023-03-10 17:19:13 -0800

    Loopo

--- Muut/Erikoinen/main.py
+++ Muut/Erikoinen/main.py
@@ -1,6 +1,29 @@
 import pygame
 import sys
 
+
+class PlayerChar:
+    x = 10
+    y = 10
+    speed = 1
+
+    def move(self, direction):
+        if direction == "right":
+            self.x = self.x + self.speed
+        elif direction == "left":
+            self.x = self.x - self.speed
+        elif direction == "up":
+            self.y = self.y - self.speed
+        elif direction == "down":
+            self.y = self.y + self.speed
+
+
+def input_tick():
+    print(f"{Player.x}, {Player.y}")
+
+
+# Game On
+Player = PlayerChar()
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
@@ -14,13 +37,14 @@ if __name__ == '__main__':
                 sys.exit()
             if event.type == pygame.KEYDOWN:  # input
                 if event.key == pygame.K_UP:
-                    print("UP")
+                    Player.move("up")
                 if event.key == pygame.K_DOWN:
-                    print("DOWN")
+                    Player.move("down")
                 if event.key == pygame.K_LEFT:
-                    print("LEFT")
+                    Player.move("left")
                 if event.key == pygame.K_RIGHT:
-                    print("RIGHT")
+                    Player.move("right")
+                input_tick()
 
         pygame.display.update()
-        # pygame.time.Clock().tick(60)
+        pygame.time.Clock().tick(30)
