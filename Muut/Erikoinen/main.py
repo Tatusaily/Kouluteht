1bc4bbd0d5d42283c5a43e8d18c0c0539c6e070f
Tatu <tatu.sailynoja@gmail.com>
2023-03-10 16:09:48 -0800

    Loopo

--- Muut/Erikoinen/main.py
+++ Muut/Erikoinen/main.py
@@ -1,11 +1,26 @@
 import pygame
+import sys
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
-    screen.fill("Black")
     print("Main")
 
     while True:
-        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("QUIT")
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:  # input
+                if event.key == pygame.K_UP:
+                    print("UP")
+                if event.key == pygame.K_DOWN:
+                    print("DOWN")
+                if event.key == pygame.K_LEFT:
+                    print("LEFT")
+                if event.key == pygame.K_RIGHT:
+                    print("RIGHT")
 
+        pygame.display.update()
+        # pygame.time.Clock().tick(60)
