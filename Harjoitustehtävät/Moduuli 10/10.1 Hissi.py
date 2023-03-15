fbce643f7fec87ba1887905180184549532b54ad
Tatusaily <tatu.sailynoja@gmail.com>
2023-03-15 14:25:37 +0200

    Mainio

--- Harjoitustehtävät/Moduuli 10/10.1 Hissi.py
+++ Harjoitustehtävät/Moduuli 10/10.1 Hissi.py
@@ -16,11 +16,11 @@ class Hissi:
     def siirry_kerrokseen(self, kerros):
         # Tarkistetaan syöte
         if kerros < self.alinkerros or kerros > self.ylinkerros:
-            print(f"{kerros}. kerros ei ole saatavilla")
+            print(f"{kerros}. kerros ei ole saatavilla\n")
             return
-        print(f"Nykyinen kerros:{self.nykyinenkerros}")
         # Siirrytään
         if kerros != self.nykyinenkerros:
+            print(f"Nykyinen kerros:{self.nykyinenkerros}")
             if kerros > self.nykyinenkerros:
                 while kerros > self.nykyinenkerros:
                     print(f"Noustaan ylöspäin...")
@@ -29,15 +29,16 @@ class Hissi:
                 while kerros < self.nykyinenkerros:
                     print(f"Laskeudutaan alaspäin...")
                     self.kerros_alas()
-            print(f"Ollaan nyt kerroksessa {self.nykyinenkerros}")
+            print(f"Ollaan nyt kerroksessa {self.nykyinenkerros}\n")
         else:
-            print(f"Hissi on jo valitussa kerroksessa.")
+            print(f"Hissi on jo valitussa kerroksessa.\n")
         return
 
 
 hissi1 = Hissi(15, 1)
 hissi1.siirry_kerrokseen(200)
 hissi1.siirry_kerrokseen(3)
+hissi1.siirry_kerrokseen(5)
 hissi1.siirry_kerrokseen(5)
 hissi1.siirry_kerrokseen(1)
 
