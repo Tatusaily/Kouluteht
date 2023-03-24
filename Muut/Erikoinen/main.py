705fc063edc28e01b35c1de77fb9c6b4f454c9cc
Tatu <tatu.sailynoja@gmail.com>
2023-03-24 23:33:00 +0200

    mato rikki

--- Muut/Erikoinen/main.py
+++ Muut/Erikoinen/main.py
@@ -1,24 +1,28 @@
 # Game On
+import random
+import pygame
+import sys
+from pygame import Vector2
+from Muut.Hymyprint import hymyprint as hymy
+
 if __name__ == '__main__':
-    import pygame
-    import sys
-    from pygame import Vector2
-    from Muut.Hymyprint import hymyprint as hymy
     # Vakiot ja bloat
     pygame.init()
     gridsize = 40
-    window_width = gridsize*10
-    window_height = gridsize*10
+    grid_width = 10
+    grid_heigth = 10
+    window_width = grid_width*gridsize
+    window_height = grid_heigth*gridsize
     screen = pygame.display.set_mode((window_width, window_height))
     gameclock = pygame.time.Clock()
     charfont = pygame.font.SysFont("Gotham", gridsize)
 
     class PlayerChar:
-        speed = gridsize
-
         def __init__(self):
             self.x = 1*gridsize/2
             self.y = 1*gridsize/2
+            self.pos = Vector2(self.x, self.y)
+            self.speed = gridsize
             # .render tekee uuden surfacen. sen voi blittaa mihin haluu
             self.glyph = charfont.render("@", True, "Green")
             self.rect = self.glyph.get_rect(center=(self.x, self.y))
@@ -45,20 +49,54 @@ if __name__ == '__main__':
 
             # Päivitetään rect oikeaan koordinaatiin
             self.rect = self.glyph.get_rect(center=(self.x, self.y))
+            self.pos = Vector2(self.x, self.y)
 
     class Snake:
-        def __init__(self, length):
+        # TODO: Mato huijaa sääntöjä ja liikkuu diagonaalisesti eikä välitä ruuduista
+        def __init__(self, length=3):
             self.body = []
-            # TODO: Kärmeelle kehonrakennus.
-            #   Käärmeelle tulee kehoon satunnainen määrä paloja. Myöhemmin enemmän?
-            #   Kaikki palat tulee samaan kohtaan:
-            #       Kun käärme lähtee liikkeelle niin se ikäänkuin kasvaa siitä pisteestä.
-            #   Sit käärmeelle suunta ja nopeus
-            #   Käärme vois alkuun liikkuu satunnaisiin suuntiin.
-            #       Myöhemmin etsii pelaajaa?
+            self.speed = gridsize
+            self.direction = Vector2(0, 0)
+            self.pos = (random.randint(0, grid_width)*gridsize, random.randint(0, grid_heigth)*gridsize)
+            for i in range(length):
+                self.body.append(Vector2(self.pos))
+            while self.direction[0] == 0 or self.direction[1] == 0:
+                self.direction = Vector2(random.randint(-1, 1), random.randint(-1, 1))
 
-            #for i in length:
-                #self.body.append(Vector2(x, y))
+        def turn(self):
+            # RNG
+            if random.randint(0, 1) == 1:
+                # Jos liike on sivuttasta; käännetään mato joko ylös tai alas.
+                if self.direction[0] == 1 or self.direction[0] == -1:
+                    self.direction[0] = 0
+                    while self.direction[1] == 0:
+                        self.direction[1] = random.randint(-1, 1)
+                # Jos liike on pystysuuntaista; käännetään matoa vasemmalle tai oikealle.
+                if self.direction[1] == 0 or self.direction[1] == -1:
+                    self.direction[1] = 0
+                    while self.direction[0] == 0:
+                        self.direction[0] = random.randint(-1, 1)
+
+
+        def move(self):
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction*self.speed)
+            self.body = body_copy[:]
+
+            if self.body[0][0] <= 0:
+                self.body[0][0] = gridsize/2
+            if self.body[0][0] > window_width:
+                self.body[0][0] = window_width - gridsize/2
+            if self.body[0][1] <= 0:
+                self.body[0][1] = gridsize/2
+            if self.body[0][1] > window_height:
+                self.body[0][1] = window_height - gridsize/2
+
+        def draw_snake(self):
+            for block in self.body:
+                body_rect = pygame.Rect(block.x, block.y, gridsize, gridsize)
+                pygame.draw.rect(screen, "Red", body_rect)
+
 
     def input_tick():
         print(f"{int(Player.x/gridsize+1)}, {int(Player.y/gridsize+1)}")
@@ -71,14 +109,22 @@ if __name__ == '__main__':
 
     print("Main")
     Player = PlayerChar()
+    Sneikki = Snake()
     testblock = pygame.Rect((4*gridsize, 4*gridsize), (gridsize, gridsize))
     hymy()
+    snake_act = pygame.USEREVENT
+    pygame.time.set_timer(snake_act, 1000)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("QUIT")
                 pygame.quit()
                 sys.exit()
+            if event.type == snake_act:
+                print(f"Sneikki: {Sneikki.body}")
+                print(f"Playa: {Player.pos}")
+                Sneikki.move()
+                Sneikki.turn()
             if event.type == pygame.KEYDOWN:  # input
                 if event.key == pygame.K_UP:
                     Player.move("up")
@@ -94,9 +140,11 @@ if __name__ == '__main__':
             Player = PlayerChar()
 
         # Draw UI
+
         screen.fill("Black")
         screen.blit(Player.glyph, Player.rect)
         pygame.draw.rect(screen, "White", testblock)
         draw_grid()
+        Sneikki.draw_snake()
 
         pygame.display.update()
