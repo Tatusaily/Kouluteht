48451fab3732ed9db225968f1df955c04606e242
Tatusaily <tatu.sailynoja@gmail.com>
2023-03-28 11:10:07 +0300

    alki

--- Harjoitustehtävät/Moduuli 13/teht1.py
+++ Harjoitustehtävät/Moduuli 13/teht1.py
@@ -4,12 +4,15 @@ Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
 Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
 Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
 """
-from flask import flask, request
-app = flask(__name__)
+from flask import Flask, request
+app = Flask(__name__)
 
 
-@app.route('/alku/<luku>')
-def alkulukutest(luku):
+@app.route('/alku')
+# TODO Palauttaa ok mut pitää laittaa virheenkäsittely
+def alkulukutest():
+    args = request.args
+    luku = int(args.get("luku"))
     on_alku = True
     # Erityiset
     if luku == 1:
@@ -23,4 +26,11 @@ def alkulukutest(luku):
                 print(f"{luku} On jaollinen luvulla {i}!")
                 on_alku = False
                 break
-    return on_alku
+    if on_alku:
+        return str("On Alkuluku")
+    else:
+        return str("Ei ole alkuluku")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
