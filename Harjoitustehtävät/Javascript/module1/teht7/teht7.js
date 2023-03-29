b7f50aaaab88c4e3b97d38eb6ff7dddcfa69b8d3
Tatu <tatu.sailynoja@gmail.com>
2023-03-29 19:04:35 +0300

    toka

---
+++ Harjoitustehtävät/Javascript/module1/teht7/teht7.js
@@ -0,0 +1,16 @@
+function roller(num){
+    let amount = parseInt(num);
+    let sum = 0;
+
+    for (let i = 0; i<amount; i++){
+        let uusinoppa = Math.floor(1+Math.random()*6)
+        sum += uusinoppa
+    }
+    document.getElementById("output").value = sum;
+}
+
+/* DEBUGIA
+let value = 5;
+let noppasumma = roller(value);
+console.log("Final noppa:" + noppasumma);
+ */
