6f99ed7df41a4b0c62f83369eabce94b62185f39
Tatu <tatu.sailynoja@gmail.com>
2023-03-29 14:52:00 +0300

    jotain testailua

---
+++ Harjoitustehtävät/Javascript/Testailu.js
@@ -0,0 +1,12 @@
+// Kaikki pääohjelmassa oleva koodi aktivoituu heti sivun avautuessa.
+// Jopa ennen HTML ja CSS koodia.
+
+function summari2() {
+    // Otetaan kaksi lukua
+    let ekaluku = parseFloat(
+        prompt("Anna ensimmäinen luku:"))
+    let tokaluku = parseFloat(
+        prompt("Anna toinen luku:"))
+    // Näytetään niiden summa
+    alert(ekaluku+tokaluku)
+}
