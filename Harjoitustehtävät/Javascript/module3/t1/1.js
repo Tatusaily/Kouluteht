0df6784d80f4a3d3bbd23ff7fb4ef03877530f3a
Tatusaily <tatu.sailynoja@gmail.com>
2023-04-11 10:55:43 +0300

    t1 tehty oikeesti

--- Harjoitustehtävät/Javascript/module3/t1/1.js
+++ Harjoitustehtävät/Javascript/module3/t1/1.js
@@ -1,5 +1,6 @@
-lista = document.getElementById("target");
-html = `<li>First item</li>
-        <li>Second item</li>
-        <li>Third item</li>`;
+const lista = document.getElementById("target")
+const html = `<li>First item</li>
+<li>Second item</li>
+<li>Third item</li>`;
 lista.innerHTML = html;
+lista.className = "my-list";
