a249726f829ec5b45349677ff25668616e7198d1
Tatu <tatu.sailynoja@gmail.com>
2023-04-19 17:05:00 +0300

    t2 tehty

--- Harjoitustehtävät/Javascript/module4/t2/t2.js
+++ Harjoitustehtävät/Javascript/module4/t2/t2.js
@@ -1,6 +1,14 @@
-function TVHakija(){
+async function TVHakija(){
     const apiUrl = "https://api.tvmaze.com/search/shows?q="
     const formQuery = `${document.getElementById("query").value}`
-    const result = fetch(apiUrl + formQuery)
-    console.log(result)
+    try {
+        const result = await fetch(apiUrl + formQuery)
+        console.log(`Lähetettiin ${apiUrl + formQuery}`)
+        const jsonResult = await result.json()
+        console.log("1")
+        console.log(jsonResult)
+    } catch(error) {
+        console.log("2")
+        console.log(error.message)
+    }
 }
