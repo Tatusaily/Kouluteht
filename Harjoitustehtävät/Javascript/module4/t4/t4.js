c1cad952529999f2c78f16f0c8bc9d1504696486
Tatu <tatu.sailynoja@gmail.com>
2023-04-23 14:22:40 +0300

    t4 = t3 muutettuna

---
+++ Harjoitustehtävät/Javascript/module4/t4/t4.js
@@ -0,0 +1,57 @@
+async function TVHakija(){
+    const apiUrl = "https://api.tvmaze.com/search/shows?q="
+    const formQuery = `${document.getElementById("query").value}`
+ try {
+        const result = await fetch(apiUrl + formQuery)
+        console.log(`Lähetettiin ${apiUrl + formQuery}`)
+        const jsonResult = await result.json()
+        console.log("1")
+        console.log(jsonResult)
+        await displayResults(jsonResult)
+   } catch(error) {
+        console.log("2")
+        console.log(error.message)
+    }
+
+
+}
+
+async function displayResults(input){
+    const input2 = input
+    console.log("3")
+    console.log(input2)
+    try {
+        document.getElementById("results").innerHTML = ""
+        input2.forEach(result => {
+            // Haetaan tiedot muuttujiin
+            const showName = result.show.name
+            const showURL = result.show.url
+            const showImg = (result.show.image !== null ? result.show.image.medium : 'https://via.placeholder.com/210x295?text=Not%20Found')
+            const showSum = result.show.summary
+
+            // Laitetaan tiedot HTML -elementteihin ja liitetään ne artikkeliin
+            const showArticle = document.createElement("article")
+            const artName = document.createElement("h2")
+            artName.innerHTML = showName
+            showArticle.appendChild(artName)
+            const artURL = document.createElement("a")
+            artURL.href = showURL
+            artURL.innerHTML = "info"
+            artURL.target = "_blank"
+            showArticle.appendChild(artURL)
+            const artIMG = document.createElement("img")
+            artIMG.src = showImg
+            artIMG.alt = showName
+            showArticle.style.backgroundImage = `url("${showImg}")`
+            showArticle.appendChild(artIMG)
+            const artSum = document.createElement("div")
+            artSum.innerHTML = showSum
+            showArticle.appendChild(artSum)
+
+            // Laitetaan artikkeli nettisivulle
+            document.getElementById("results").appendChild(showArticle)
+        })
+    }catch (error){
+        console.log(error.message)
+    }
+}
