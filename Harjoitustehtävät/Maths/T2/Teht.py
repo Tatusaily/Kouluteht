490e23ebf4314c4dc1e6c92f4dff3f2c18261a6a
Tatu <tatu.sailynoja@gmail.com>
2023-09-01 14:24:04 +0300

    valmis

--- Harjoitustehtävät/Maths/T2/Teht.py
+++ Harjoitustehtävät/Maths/T2/Teht.py
@@ -1,10 +1,24 @@
 import numpy as np
-import random
 
 # T1
-lista = np.array([])
-for i in range(5):
-    num = random.randint(1, 9)
-    np.append(lista, num)
+t1 = np.random.randint(9, size=5)
+print(f"{'T1':-^15}\n"
+      f"{t1}\n"
+      f"{'':-^15}\n")
 
-print(lista)
+# T2
+t2_a = np.array([[2, 3], [4, -7]])
+t2_b = np.array([[1, 1, 1], [3, -3, 2]])
+print(f"{'T2':-^15}\n"
+      f"a) {t2_a}\n\n"
+      f"b) {t2_b}\n"
+      f"{'':-^15}\n")
+
+# T3
+print(f"{'T3':-^15}\n"
+      f"{'Normit:':<15}")
+print(f"{'u':<3} = {np.linalg.norm(t2_a[0]):>8.3f}\n"
+      f"{'v':<3} = {np.linalg.norm(t2_a[1]):>8.3f}\n"
+      f"{'uu':<3} = {np.linalg.norm(t2_b[0]):>8.3f}\n"
+      f"{'vv':<3} = {np.linalg.norm(t2_a[1]):>8.3f}\n")
+print(f"{'':-^15}")
